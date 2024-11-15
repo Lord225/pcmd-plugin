d62ff4bb119bce763072e74389c5a5f2e6b6d002
Lord225 <zlotymaciej@gmail.com>
2024-11-15 15:04:15 +0100

    stuff

--- src/main/kotlin/pl/redstonefun/pcmd/commands/PcmdCommand.kt
+++ src/main/kotlin/pl/redstonefun/pcmd/commands/PcmdCommand.kt
@@ -2,13 +2,18 @@ package pl.redstonefun.pcmd.commands
 
 import com.google.gson.GsonBuilder
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import org.bukkit.NamespacedKey
 import org.bukkit.command.Command
+import org.bukkit.Material
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.ItemMeta
+import org.bukkit.persistence.PersistentDataType
 import pl.redstonefun.pcmd.PCMD
 
 class PcmdCommand(private val plugin: PCMD) : Command("pcmd") {
@@ -16,6 +21,10 @@ class PcmdCommand(private val plugin: PCMD) : Command("pcmd") {
     init {
         description = "Sends a hello message to the player."
         usageMessage = "/hello"
+    }
+
+    companion object {
+        val PCMD_KEY = NamespacedKey("pcmd", "pcmd")
     }
 //
 //    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
@@ -41,6 +50,8 @@ class PcmdCommand(private val plugin: PCMD) : Command("pcmd") {
 //
 //        return true
 //    }
+
+    fun usageMessage() = Component.text("Użycie: /pcmd all/me <tekst>").color(NamedTextColor.RED)
 
     fun sanitzeText(text: Component): Component {
         // remove all events that can run commands
@@ -69,16 +80,30 @@ class PcmdCommand(private val plugin: PCMD) : Command("pcmd") {
         }
     }
 
+    fun getTellrawCommand(text: Component, selector: String): String {
+        val jsonText = GsonComponentSerializer.gson().serialize(text)
+        return "/tellraw $selector $jsonText"
+    }
+
 
 
     override fun execute(sender: CommandSender, label: String, args: Array<String>): Boolean {
         if (sender !is Player) return false
         if (args.isEmpty()) {
-            sender.sendMessage("§cUżycie: /pcmd <tekst>")
+            sender.sendMessage(usageMessage())
             return true
         }
 
-        val rawText = args.joinToString(" ")
+        val selector = when(args[0]) {
+            "all" -> "@a[r=100]"
+            "me" -> sender.name
+            else -> {
+                sender.sendMessage(usageMessage())
+                return true
+            }
+        }
+
+        val rawText = args.drop(1).joinToString(" ")
 
         val jsonText = getFromJsonText(rawText)
         val legacyText = getFromLegacyText(rawText)
@@ -90,6 +115,19 @@ class PcmdCommand(private val plugin: PCMD) : Command("pcmd") {
         } else {
             Component.text(rawText)
         }
+
+        val command = getTellrawCommand(sanitizedMessage, selector)
+
+        val item = ItemStack(Material.GOLD_BLOCK).apply {
+            itemMeta = itemMeta?.also { meta ->
+                meta.displayName(sanitizedMessage)
+                meta.persistentDataContainer.set(PCMD_KEY, PersistentDataType.STRING, command)
+            }
+        }
+
+        println("User ${sender.name} created PCMD: $command")
+
+        sender.inventory.addItem(item)
 
         sender.sendMessage(sanitizedMessage)
 
